47e55e545582daecc22679af2e6cb6fd547709b1
Henrique <henrique.saura@gmail.com>
2024-08-16 21:54:08 +1000

    isAuthenticated removed

--- analysis/urls.py
+++ analysis/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from analysis import views
 
 urlpatterns = [
-    re_path(r'^products/$', views.Home.as_view()),
-    re_path(r'^products/([0-9]+)$', views.Home.as_view()),
+    re_path(r'^products/$', views.productsApi),
+    re_path(r'^products/([0-9]+)$', views.productsApi),
 ]
--- analysis/views.py
+++ analysis/views.py
@@ -2,45 +2,37 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
 from django.http.response import JsonResponse
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.decorators import permission_classes
-from rest_framework.views import APIView
-from rest_framework_simplejwt.authentication import JWTAuthentication
-from django.http import HttpResponse
+
 
 from analysis.models import Products, Orders, OrderedItems
 from analysis.serializers import ProductsSerializer, OrdersSerializer, OrderedItemsSerializer
 
 
-#@csrf_exempt
-#@permission_classes([IsAuthenticated])
+@csrf_exempt
 
-class Home(APIView):
-    authentication_classes = [JWTAuthentication]
-    permission_classes = [IsAuthenticated]
-    def get(self, request):
-        if request.method == 'GET':
-            products = Products.objects.all()
-            products_serializer = ProductsSerializer(products, many=True)
-            return HttpResponse(JsonResponse(products_serializer.data, safe=False))
-        elif request.method == 'POST':
-            product_data = JSONParser().parse(request)
-            products_serializer = ProductsSerializer(data=product_data)
-            if products_serializer.is_valid():
-                products_serializer.save()
-                return HttpResponse(JsonResponse(products_serializer.data, safe=False))
-            return JsonResponse("Failed to Add", safe=False)
-        elif request.method == 'PUT':
-            product_data = JSONParser().parse(request)
-            product = Products.objects.get(product_id=product_data['product_id'])
-            products_serializer = ProductsSerializer(product, data=product_data)
-            if products_serializer.is_valid():
-                products_serializer.save()
-                return HttpResponse(JsonResponse(products_serializer.data, safe=False))
-            return JsonResponse("Failed to Update", safe=False)
-        elif request.method == 'DELETE':
-            product = Products.objects.get(product_id=id)
-            product.delete()
-            return HttpResponse(JsonResponse("Deleted Successfully", safe=False))
+def productsApi(request, id=0):
+    if request.method == 'GET':
+        products = Products.objects.all()
+        products_serializer = ProductsSerializer(products, many=True)
+        return JsonResponse(products_serializer.data, safe=False)
+    elif request.method == 'POST':
+        product_data = JSONParser().parse(request)
+        products_serializer = ProductsSerializer(data=product_data)
+        if products_serializer.is_valid():
+            products_serializer.save()
+            return JsonResponse(products_serializer.data, safe=False)
+        return JsonResponse("Failed to Add", safe=False)
+    elif request.method == 'PUT':
+        product_data = JSONParser().parse(request)
+        product = Products.objects.get(product_id=product_data['product_id'])
+        products_serializer = ProductsSerializer(product, data=product_data)
+        if products_serializer.is_valid():
+            products_serializer.save()
+            return JsonResponse(products_serializer.data, safe=False)
+        return JsonResponse("Failed to Update", safe=False)
+    elif request.method == 'DELETE':
+        product = Products.objects.get(product_id=id)
+        product.delete()
+        return JsonResponse("Deleted Successfully", safe=False)
 
-    # Create your views here.
+# Create your views here.
