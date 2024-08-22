4de52fee2275ea3a93f8fa133de7f14ed88ffe0e
Henrique <henrique.saura@gmail.com>
2024-08-22 18:18:44 +1000

    added user-countries to all apis

--- analysis/views.py
+++ analysis/views.py
@@ -118,10 +118,10 @@ def orderStatusCountApi(request):
     if request.method == 'GET':
         with connection.cursor() as cursor:
             cursor.execute("""
-                SELECT status, COUNT(*) AS order_count
+                SELECT user_country, status, COUNT(*) AS order_count
                 FROM analysis_orders
-                GROUP BY status
-                ORDER BY order_count DESC;
+                GROUP BY user_country, status
+                ORDER BY user_country, order_count DESC;
             """)
             rows = cursor.fetchall()
         
@@ -129,8 +129,9 @@ def orderStatusCountApi(request):
         status_counts = []
         for row in rows:
             status_counts.append({
-                "status": row[0],
-                "count": row[1]
+                "user_country": row[0],
+                "status": row[1],
+                "count": row[2]
             })
 
         return JsonResponse(status_counts, safe=False)
@@ -141,11 +142,12 @@ def totalItemsSoldApi(request):
     if request.method == 'GET':
         with connection.cursor() as cursor:
             cursor.execute("""
-                SELECT ap.product_id, ap.name AS product_name, SUM(aoi.count) AS total_items_sold
+                SELECT ao.user_country, ap.product_id, ap.name AS product_name, SUM(aoi.count) AS total_items_sold
                 FROM analysis_ordereditems aoi
                 JOIN analysis_products ap ON aoi.product_id_id = ap.product_id
-                GROUP BY ap.product_id, ap.name
-                ORDER BY total_items_sold DESC;
+                JOIN analysis_orders ao on ao.order_id = aoi.order_id_id
+                GROUP BY ao.user_country, ap.product_id, ap.name
+                ORDER BY ao.user_country, total_items_sold DESC;
             """)
             rows = cursor.fetchall()
         
@@ -153,9 +155,10 @@ def totalItemsSoldApi(request):
         total_sold = []
         for row in rows:
             total_sold.append({
-                "id": row[0],
-                "product_name": row[1],
-                "total_sold": row[2]
+                "user_country": row[0],
+                "id": row[1],
+                "product_name": row[2],
+                "total_sold": row[3]
             })
 
         return JsonResponse(total_sold, safe=False)
@@ -166,11 +169,11 @@ def statusesByMonths(request):
     if request.method == 'GET':
         with connection.cursor() as cursor:
             cursor.execute("""
-                SELECT DATE_FORMAT(ao.timestamp, '%b %y') AS month, ao.status, COUNT(*) AS order_count
+                SELECT ao.user_country, DATE_FORMAT(ao.timestamp, '%b %y') AS month, ao.status, COUNT(*) AS order_count
                 FROM analysis_orders ao
                 WHERE ao.timestamp >= DATE_SUB(CURDATE(), INTERVAL 1 YEAR)
-                GROUP BY YEAR(ao.timestamp), MONTH(ao.timestamp), ao.status
-                ORDER BY YEAR(ao.timestamp) ASC, MONTH(ao.timestamp) ASC, ao.status ASC;
+                GROUP BY ao.user_country, YEAR(ao.timestamp), MONTH(ao.timestamp), ao.status
+                ORDER BY ao.user_country, YEAR(ao.timestamp) ASC, MONTH(ao.timestamp) ASC, order_count DESC, ao.status ASC;
             """)
             rows = cursor.fetchall()
         
@@ -178,9 +181,10 @@ def statusesByMonths(request):
         statuses = []
         for row in rows:
             statuses.append({
-                "month": row[0],
-                "status": row[1],
-                "count": row[2]
+                "user_country": row[0],
+                "month": row[1],
+                "status": row[2],
+                "count": row[3]
             })
 
         return JsonResponse(statuses, safe=False)
